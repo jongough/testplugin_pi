e63690e5b9d18fd02138a06a5c56d9a41a008fbc
jongough <jon.gough@eclipsesystems.com.au>
2019-11-16 16:54:53 +1100

    Try adding leamas/opencpn-mingw

--- ci/circleci-build-mingw.sh
+++ ci/circleci-build-mingw.sh
@@ -6,8 +6,10 @@
 
 set -xe
 
-su -c "dnf install -q -y sudo dnf-plugins-core"
-sudo dnf -q builddep  -y mingw/fedora/opencpn-deps.spec
+#su -c "dnf install -q -y sudo dnf-plugins-core"
+sudo dnf copr enable leamas/opencpn-mingw
+sudo dnf builddep opencpn-deps.spec
+#sudo dnf -q builddep  -y mingw/fedora/opencpn-deps.spec
 rm -rf build; mkdir build; cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=../mingw/fedora/toolchain.cmake ..
 make -j2
