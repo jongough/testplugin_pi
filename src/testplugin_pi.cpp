99b791ad3d79642b4f4cb7ac20385a8252b10f76
jongough <jon.gough@eclipsesystems.com.au>
2020-02-18 13:47:22 +1100

    Update to use COMMON_NAME to sort out an issue with XML and the name returned to OCPN

--- src/testplugin_pi.cpp
+++ src/testplugin_pi.cpp
@@ -337,7 +337,7 @@ int testplugin_pi::GetPlugInVersionMinor()
 
 wxString testplugin_pi::GetCommonName()
 {
-    return _(PLUGIN_COMMON_NAME);
+    return _T(PLUGIN_COMMON_NAME);
 }
 
 wxString testplugin_pi::GetShortDescription()
