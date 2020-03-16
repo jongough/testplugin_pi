41d51af8c3b048371591857e4f76eff9e9138125
jongough <jon.gough@eclipsesystems.com.au>
2020-03-16 18:14:52 +1100

    Try installing packages with gdebi as apt-get in trusty does not support installing from local deb file

--- ci/circleci-build-trusty.sh
+++ ci/circleci-build-trusty.sh
@@ -5,12 +5,11 @@
 #
 set -xe
 sudo apt-get -qq update
-sudo apt-get install devscripts
-sudo apt-get install equivs
+sudo apt-get install devscripts equivs gdebi
 
 rm -rf build && mkdir build && cd build
 mk-build-deps ../ci/control
-sudo apt-get install  ./*all.deb  || :
+sudo gdebi -n ./*all.deb  || :
 sudo apt-get --allow-unauthenticated install -f
 rm -f ./*all.deb
 
