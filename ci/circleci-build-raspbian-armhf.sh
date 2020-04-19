4420a601ae317d56272c0c975b645f1cc2fbc80d
jongough <jon.gough@eclipsesystems.com.au>
2020-04-19 12:44:17 +1000

    remove tail command

--- ci/circleci-build-raspbian-armhf.sh
+++ ci/circleci-build-raspbian-armhf.sh
@@ -32,7 +32,7 @@ docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker run --privileged -d -ti -e "container=docker" \
       -v ~/source_top:/source_top \
       -v $(pwd):/ci-source:rw \
-      $DOCKER_IMAGE /bin/bash tail -f /dev/null
+      $DOCKER_IMAGE /bin/bash
 DOCKER_CONTAINER_ID=$(docker ps | grep raspbian | awk '{print $1}')
 sleep 5;
 #docker exec -ti $DOCKER_CONTAINER_ID sudo apt-get update
