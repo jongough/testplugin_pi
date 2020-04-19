1fd22396101aaef4b7476d58f960cf5363ab6508
jongough <jon.gough@eclipsesystems.com.au>
2020-04-19 17:00:45 +1000

    Update image

--- ci/circleci-build-raspbian-armhf.sh
+++ ci/circleci-build-raspbian-armhf.sh
@@ -17,6 +17,10 @@ docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker run --privileged -d -ti -e "container=docker"  -v $(pwd):/ci-source:rw $DOCKER_IMAGE /bin/bash
 DOCKER_CONTAINER_ID=$(docker ps | grep raspbian | awk '{print $1}')
 
+docker exec -ti $DOCKER_CONTAINER_ID apt-get update
+docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install git cmake build-essential cmake gettext wx-common libgtk2.0-dev libwxgtk3.0-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev lsb-release
+
+
 echo $OCPN_TARGET
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     "export CICLECI=$CIRCLECI; export CIRCLECI_BRANCH=$CIRCLECI_BRANCH; export OCPN_TARGET=$OCPN_TARGET; rm -rf ci-source/build; mkdir ci-source/build; cd ci-source/build; cmake ..; make $BUILD_FLAGS; make package; chmod -R a+rw ../build;"
