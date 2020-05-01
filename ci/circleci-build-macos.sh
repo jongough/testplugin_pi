066fb626a200578578c4dcbf19cc191a2008877e
jongough <jon.gough@eclipsesystems.com.au>
2020-05-01 17:28:38 +1000

    Fix for location of macos build [skip appveyor] [skip travis]

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -12,12 +12,9 @@ fi
 set -xe
 
 set -o pipefail
-for pkg in cairo libexif xz libarchive wget cmake; do
+for pkg in cairo cmake libarchive libexif python3 wget; do
     brew list $pkg 2>/dev/null | head -10 || brew install $pkg
 done
-brew unlink python@2
-brew upgrade python
-brew ls python3
 
 wget -q http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
 tar xJf wx312_opencpn50_macos109.tar.xz -C /tmp
@@ -25,12 +22,12 @@ export PATH="/usr/local/opt/gettext/bin:$PATH"
 echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
 
 rm -rf build && mkdir build && cd build
-test -z "$TRAVIS_TAG" && CI_BUILD=OFF || CI_BUILD=ON
-cmake -DOCPN_CI_BUILD=$CI_BUILD \
+cmake \
   -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312_opencpn50_macos109/bin/wx-config \
   -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312_opencpn50_macos109" \
-  -DCMAKE_INSTALL_PREFIX="/" \
+  -DCMAKE_INSTALL_PREFIX= \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+  "/" \
   ..
 make -sj2
 make package
@@ -38,6 +35,5 @@ make package
 wget -q http://opencpn.navnux.org/build_deps/Packages.dmg
 hdiutil attach Packages.dmg
 sudo installer -pkg "/Volumes/Packages 1.2.5/Install Packages.pkg" -target "/"
-#make install
 make create-pkg
 
