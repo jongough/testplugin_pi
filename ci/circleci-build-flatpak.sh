0ccf742886b9f8f616add92ed1432e1ccbf40fcc
jongough <jon.gough@eclipsesystems.com.au>
2020-05-15 10:38:39 +1000

    Update to provide git server

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -54,6 +54,7 @@ if [ -n "$CIRCLECI" ]; then
     export CIRCLE_TAG=\"$CIRCLE_TAG\";
     export CIRCLE_PROJECT_USERNAME=\"$CIRCLE_PROJECT_USERNAME\";
     export CIRCLE_PROJECT_REPONAME=\"$CIRCLE_PROJECT_REPONAME\";
+    export GIT_REPOSITORY_SERVER=\"$GIT_REPOSITORY_SERVER\";
     export OCPN_TARGET=$OCPN_TARGET;
     bash -xe $TOPDIR/ci/docker-build-flatpak.sh 28;
          echo -ne \"------\nEND OPENCPN-CI BUILD\n\";"
