5d3b5e2987904a9ec4a7f3023f14997ee5c920ca
jongough <jon.gough@eclipsesystems.com.au>
2020-12-11 19:05:30 +1100

    See if duckdns is working yet

--- ci/docker-build-flatpak.sh
+++ ci/docker-build-flatpak.sh
@@ -11,11 +11,11 @@ cd $TOPDIR
 su -c "dnf install -y sudo cmake gcc-c++ flatpak-builder flatpak make tar"
 flatpak remote-add --user --if-not-exists flathub \
     https://flathub.org/repo/flathub.flatpakrepo
-#ocpnfound=$(flatpak list | grep org.opencpn.OpenCPN | awk '{print $1}')
-#if [ "" = "$ocpnfound" ]; then
-#   flatpak install --user  -y \
-#       http://opencpn.duckdns.org/opencpn/opencpn.flatpakref
-#fi
+ocpnfound=$(flatpak list | grep org.opencpn.OpenCPN | awk '{print $1}')
+if [ "" = "$ocpnfound" ]; then
+   flatpak install --user  -y \
+       http://opencpn.duckdns.org/opencpn/opencpn.flatpakref
+fi
 flatpak install --user -y flathub org.opencpn.OpenCPN
 
 flatpak install --user -y  flathub org.freedesktop.Sdk//18.08
