dbbfa4bc0d31eeaa182cde362815fadd9807faed
jongough <jon.gough@eclipsesystems.com.au>
2021-01-31 14:43:12 +1100

    Split input to apt depending on platform [skip appveyor] [skip travis]

--- ci/circleci-build-raspbian-armhf.sh
+++ ci/circleci-build-raspbian-armhf.sh
@@ -36,13 +36,18 @@ docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     -e "BUILD_GTK3=$BUILD_GTK3"
 
 # Run build script
-cat > build.sh << "EOF"
-#apt-get -q update
-#apt install -y curl gnupg
-#curl http://mirrordirector.raspbian.org/raspbian.public.key  > raspikey
-#apt-key add raspikey
-#curl http://archive.raspbian.org/raspbian.public.key  > raspikey
-#apt-key add raspikey
+if [ "$BUILD_ENV" = "raspbian" ];
+    cat > build.sh << "EOF"
+    apt-get -q update
+    apt install -y curl gnupg
+    curl http://mirrordirector.raspbian.org/raspbian.public.key  > raspikey
+    apt-key add raspikey
+    curl http://archive.raspbian.org/raspbian.public.key  > raspikey
+    apt-key add raspikey
+    EOF
+fi
+
+cat >> build.sh << "EOF"
 apt -q update
 apt-get -y install --no-install-recommends \
     git cmake build-essential cmake gettext wx-common libgtk2.0-dev libwxgtk3.0-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev lsb-release
