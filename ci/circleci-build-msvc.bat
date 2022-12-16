8103940f3ec55f056f91e7b945f1c8cd69bd058b
jongough <jon.gough@eclipsesystems.com.au>
2022-12-16 15:07:51 +1100

    fix typo

--- ci/circleci-build-msvc.bat
+++ ci/circleci-build-msvc.bat
@@ -5,7 +5,7 @@ set "SCRIPTDIR=%~dp0"
 set "GIT_HOME=C:\Program Files\Git"
 if "%CONFIGURATION%" == "" set "CONFIGURATION=RelWithDebInfo"
 
-set wx_vers="wx%WXVERSION""
+set wx_vers="wx%WXVERSION%"
 echo Building %wx_vers%
 
 call %SCRIPTDIR%..\msvc\win_deps.bat %wx_vers%
