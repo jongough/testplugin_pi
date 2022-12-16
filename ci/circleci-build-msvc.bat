b56961fd08bfed7f5244016f4a82cd685d650004
jongough <jon.gough@eclipsesystems.com.au>
2022-12-16 17:24:20 +1100

    Fix typo

--- ci/circleci-build-msvc.bat
+++ ci/circleci-build-msvc.bat
@@ -13,7 +13,7 @@ dir %SCRIPTDIR%\..
 dir %SCRIPTDIR%\..\msvc
 call "%SCRIPTDIR%\..\msvc\win_deps" %wx_vers%
 path %SCRIPTDIR%\.local\bin;%PATH%;"C:\Program Files\CMake\bin"
-path dir "C:\Program Files\CMake\bin"
+dir "C:\Program Files\CMake\bin"
 call %SCRIPTDIR%..\cache\wx-config.bat
 path
 echo USING wxWidgets_LIB_DIR: !wxWidgets_LIB_DIR!
