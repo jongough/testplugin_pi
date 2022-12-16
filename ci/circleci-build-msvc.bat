1fa1981b574345148143f05114c23d4a012f21c8
jongough <jon.gough@eclipsesystems.com.au>
2022-12-16 12:12:54 +1100

    use new msvc directory to build

--- ci/circleci-build-msvc.bat
+++ ci/circleci-build-msvc.bat
@@ -1,5 +1,33 @@
 #
-# Build the Trusty Ubuntu artifacts
+# Build the msvc artifacts
 #
-set "VS_HOME=C:\Program Files\Microsoft Visual Studio\2022"
-call "%VS_HOME%\Community\VC\Auxiliary\Build\vcvars32.bat"
+set "SCRIPTDIR=%~dp0"
+set "GIT_HOME=C:\Program Files\Git"
+if "%CONFIGURATION%" == "" set "CONFIGURATION=RelWithDebInfo"
+
+set "wx_vers=wx32"
+echo Building !wx_vers!
+
+call %SCRIPTDIR%..\msvc\win_deps.bat !wx_vers!
+call %SCRIPTDIR%..\cache\wx-config.bat
+echo USING wxWidgets_LIB_DIR: !wxWidgets_LIB_DIR!
+echo USING wxWidgets_ROOT_DIR: !wxWidgets_ROOT_DIR!
+echo USING OCPN_TARGET_TUPLE: !TARGET_TUPLE!
+
+nmake /?  >nul 2>&1
+if errorlevel 1 (
+  set "VS_HOME=C:\Program Files\Microsoft Visual Studio\2022"
+  call "%VS_HOME%\Community\VC\Auxiliary\Build\vcvars32.bat"
+)
+
+if exist build (rmdir /s /q build)
+mkdir build && cd build
+
+cmake -A Win32 -G "Visual Studio 17 2022" ^
+    -DCMAKE_GENERATOR_PLATFORM=Win32 ^
+    -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
+    -DwxWidgets_LIB_DIR=!wxWidgets_LIB_DIR! ^
+    -DwxWidgets_ROOT_DIR=!wxWidgets_ROOT_DIR! ^
+    -DOCPN_TARGET_TUPLE=!TARGET_TUPLE! ^
+    ..
+cmake --build . --target tarball --config %CONFIGURATION%
