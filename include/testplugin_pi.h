bf5f7b56c134cd7fc65a3d5ed398f55fb7c217a2
Dave <bdbcat@yahoo.com>
2023-09-15 11:40:53 -0400

    Remove even more unnecessary GL header file references

--- include/testplugin_pi.h
+++ include/testplugin_pi.h
@@ -130,10 +130,10 @@ std::cout << x  << std::endl ; } while (0)
 #endif
 
 #ifndef __OCPN__ANDROID__
-#include <GL/gl.h>
+//#include <GL/gl.h>
 //#include <GL/glu.h>
 #else
-#include "qopengl.h"                  // this gives us the qt runtime gles2.h
+//#include "qopengl.h"                  // this gives us the qt runtime gles2.h
 //#include "GL/gl_private.h"
 #endif
 #endif
