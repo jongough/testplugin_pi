a964c57cf9435d9a1909ddc047a990141b8cc213
jongough <2803240+jongough@users.noreply.github.com>
2025-01-13 08:25:59 +1100

    Try making macos libs search in the application folder

--- ci/circleci-build-macos-universal.sh
+++ ci/circleci-build-macos-universal.sh
@@ -74,12 +74,12 @@ fi
 
 # nor-reproducible error on first invocation, seemingly tarball-conf-stamp
 # is not created as required.
-#make VERBOSE=1 tarball || make VERBOSE=1 tarball
+make VERBOSE=1 tarball || make VERBOSE=1 tarball
 #make
-#make install
+#make install || make install
 #make package
 #make package
-make package || make package
+#make package || make package
 
 # Create the cached /usr/local archive
 if [ -n "$CI"  ]; then
