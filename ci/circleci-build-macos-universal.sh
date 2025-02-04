6709def9b5511c51ace726fbea5c9a05f10695a1
jongough <2803240+jongough@users.noreply.github.com>
2025-02-04 13:21:10 +1100

    Put make install before make package to see if it resolves a problem with some builds on macOS

--- ci/circleci-build-macos-universal.sh
+++ ci/circleci-build-macos-universal.sh
@@ -74,6 +74,7 @@ fi
 
 # nor-reproducible error on first invocation, seemingly tarball-conf-stamp
 # is not created as required.
+make install || make install
 make package || make package
 
 # Create the cached /usr/local archive
